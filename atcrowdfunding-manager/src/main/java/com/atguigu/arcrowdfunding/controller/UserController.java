58bbea4bade0d4081d0f84d719094a52ceef9f7a
yintc <yintc@ulopay.com>
2019-10-09 15:02:17 +0800

    1

--- atcrowdfunding-manager/src/main/java/com/atguigu/arcrowdfunding/controller/UserController.java
+++ atcrowdfunding-manager/src/main/java/com/atguigu/arcrowdfunding/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.atguigu.arcrowdfunding.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+
 }
